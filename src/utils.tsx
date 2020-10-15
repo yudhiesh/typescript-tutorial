32bb3fecf5e218f82ba570b20bcd75cb4e95421d
Yudhiesh Ravindranath <yudhiesh1997@gmail.com>
2020-10-15 11:57:08 +0800

    Converted Utils to tsx

--- src/utils.tsx
+++ src/utils.tsx
@@ -1,4 +1,13 @@
-export const login = async ({ username, password }) => {
+//When destructuring a variable you need to add an explicit declaration of the types
+//associated with the paramaters again
+
+export const login = async ({
+    username,
+    password
+}: {
+    username: string;
+    password: string;
+}) => {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
             if (username === "harry" && password === "password") {
